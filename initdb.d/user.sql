b1e2b860dbfd522b6e0a89d521696ac90d084352
KeN <ken.t.csai@gmail.com>
2023-02-17 01:11:14 +0900

    create docker-compose.yml

---
+++ initdb.d/user.sql
@@ -0,0 +1,14 @@
+USE bulletin_board;
+
+CREATE TABLE IF NOT EXISTS Users (
+  id           INT NOT NULL AUTO_INCREMENT,
+  username     VARCHAR(30) NOT NULL DEFAULT '',
+  mail_address VARCHAR(30) NOT NULL DEFAULT '',
+  status       VARCHAR(10) NOT NULL DEFAULT 'DISABLE',
+  create_at    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_at    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO Users (username, mail_address, status)
+VALUES ('Ken', 'abc@example.com', 'ENABLE');
